7628b06d75671a73237bfc837367b9d80aabaac1
hayleethegamer <hayleethegamer@gmail.com>
2016-07-20 18:20:00 -0400

    Add files via upload

--- Twitch_Moding.py
+++ Twitch_Moding.py
@@ -1,11 +1,28 @@
-from Functions import sendMessage, varChange, fileRead, fileWrite,removeLine, capsModding
+from Functions import sendMessage, varChange, fileRead, fileWrite,removeLine, capsModding, getPartTime
+from Initalize import lastHourStart, lastMinuteStart
+spamDetection = []
+spamDetectionNums = []
+started = False
+if started == False:
+	lastMinute = lastMinuteStart
+	lastHour = lastHourStart
+	started = True
 async def twitchModing(message,message2,client,user,platform,trusted,tempWhitelist,mods,channel,autoCounter):
+	global spamDetection
+	global spamDetectionNums
+	global lastMinute
+	global lastHour
+	spam = spamDetection(message,message2,client,user,platform,spamDetection,spamDetectionNums)
+	if spam == True:
+		return
 	if " n.i.g.g.e.r" in message:
 		await sendMessage(message,client,platform, "/ban " + user)
+		return
 	elif "ACTION" in message:
 		if "donated" in message.lower(): #("has donated " in message.lower()) and ("to the stream with message" in message.lower()) or ("has donated" in message.lower() or 
 			await sendMessage(message,client,platform, "/timeout " + user + " 600")
 			await sendMessage(message,client,platform, "Don't do fake donations please, chill for 10 mintues")
+			return
 	elif (user not in trusted) and (user not in mods):
 		if user not in tempWhitelist:
 			linkKey = fileRead("LinkKeys.txt").split()
@@ -16,6 +33,7 @@ async def twitchModing(message,message2,client,user,platform,trusted,tempWhiteli
 					await sendMessage(message,client,platform, "Sorry, no links please, if you want to post a link ask a mod")
 					break
 				count = count + 1
+			return
 		if len(message2) >= 10:
 			split = message2.split()
 			noSpaceMessage = "".join(split)
@@ -28,55 +46,67 @@ async def twitchModing(message,message2,client,user,platform,trusted,tempWhiteli
 			#print (str(percent) + "%")
 			if (len(message2) >= 10) and (percent >= 90) or (len(message2) >= 15) and (percent >= 80) or (len(message2) >= 20) and (percent >= 70):
 				await capsModding(message,message2,client,user,platform,channel)
-	puncutation = [",",".","?","!","'",'"',"*",":","`","~","_","@","#",";",":"]
-	puncutation = "".join(puncutation)
-	blacklist = fileRead("BlackList.txt").split()
-	count = 0
-	messCount = 0
-	splitMessage = message.lower().split()
-	#print(splitMessage)
-	while messCount <= len(splitMessage)-1:
-		#print("mess" + str(messCount))
-		filteredMessage = splitMessage[messCount].strip(puncutation)
-		filteredSplit = list(filteredMessage)
-		#print(filteredSplit)
-		filterCount = 0
-		filteredMessageArray = []
-		while filterCount <= len(filteredSplit)-1:
-			if filteredSplit[filterCount] not in puncutation:
-				filteredMessageArray.append(filteredSplit[filterCount])
-				#print(filteredMessageArray)
-				#print(filterCount)
-				#print("filteredwhile")
-			filterCount = filterCount + 1
-		filteredMessage = "".join(filteredMessageArray)
-		while count <= len(blacklist)-1:
-			#print(len(splitMessage))
-			#print("count" + str(count))
-			#print(blacklist[count])
-			if blacklist[count] == filteredMessage:
-				await sendMessage(message,client,platform, "/timeout " + user + " 10")
-				await sendMessage(message,client,platform, "Don't say that, take 10 seconds to chill.")
-				break
-			count = count + 1
-		messCount = messCount + 1
+				return
+	puncutation = set([",",".","?","!","'",'"',"*",":","`","~","_","@","#",";",":"])
+	blacklist = set(fileRead(channel + "/BlackList.txt").split())
+	filteredMessage = "".join(x for x in message if x not in puncutation)
+	filteredMessage = set(filteredMessage.lower().split())
+	#print(blacklist)
+	#print(filteredMessage)
+	if len(filteredMessage.intersection(blacklist)) > 0:
+		await sendMessage(message,client,platform, "/timeout " + user + " 10")
+		await sendMessage(message,client,platform, "Don't say that, take 10 seconds to chill.")
+		return
 	#automated responces
+	if channel == "Haylee":
+		chestGreyList = set(fileRead(channel + "/ChestGreyList.txt").split())
+		bottomGreyList = set(fileRead(channel + "/BottomGreyList.txt").split())
+		if len(filteredMessage.intersection(chestGreyList)) > 0:
+			await sendMessage(message,client,platform, "Sorry your jealous " + user +", but... 'Guys have them too, theyre just not as cool' ~[Name omitted for privacy] April 1, 2015")
+			return
+		elif len(filteredMessage.intersection(bottomGreyList)) > 0:
+			await sendMessage(message,client,platform, "Hey, don't hate cause there ass is better then your's" + user + ".")
+			return
+		print("test")
 	if channel == "John":
 		if ("can i join" in message.lower()) or ("could i join" in message.lower()) or ("can i play" in message.lower()) or ("can i play with you" in message.lower() or ("may I join" in message.lower())):
 			if autoCounter[0] == 0:
 				await sendMessage(message,client,platform, "Sorry " + user + ", but this is a solo world")
 				autoCounter[0] = 10
+				return
 		elif ("can i be mod" in message.lower()) or ("may i be mod" in message.lower()) or ("how do i become a mod" in message.lower()):
 			if autoCounter[1] == 0:
 				await sendMessage(message,client,platform, "you want to be a mod " + user + "? try to lift mjolnir. Can't do it? Didn't think so. Foolish mortals")
 				autoCounter[1] = 10
+				return
 		elif ("what should john do" in message.lower()):
 			if autoCounter[2] == 0:
 				await sendMessage(message,client,platform, "CALL THE AVENGERS")
 				autoCounter[2] = 10
+				return
 
 
-
+async def spamDetection(message,message2,client,user,platform,spamDetection,spamDetectionNums,lastHour,lastMinute):
+	global lastMinute
+	global lastHour
+	global spamDetection
+	global spamDetectionNums
+	if lastMinute = getPartTime("minute"):
+		spamDetectionSet = set(spamDetection)
+		userList = []
+		userList.append(user)
+		userList = set(userList)
+		if len(user.intersection(spamDetection)) > 0:
+			common = userList.intersection(spamDetectionSet)
+			common = list(common)
+			indexNum = array.index(common[0])
+			if spamDetectionNums[indexNum] > 30:
+				await sendMessage(message,client,platform,"/timeout " + user + "60")
+				await sendMessage(message,client,platform,"Sorry " + user + " but we do not like spam here")
+				return True
+	else:
+		spamDetectionNums = []
+		spamDetection = []
 
 async def twitchOnlyCommands(message,message2,client,user,platform,trusted,channel,commandCounter):
 	if (message == "!timeoutm\r") and (commandCounter[0] == 0):
